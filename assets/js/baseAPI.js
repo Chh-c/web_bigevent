7350df9ebe3ec34d4caea2fe5edb51adc2e48e34
chenhaha <1547545118@qq.com>
2021-10-24 23:26:59 +0800

    登录注册功能完成

---
+++ assets/js/baseAPI.js
@@ -0,0 +1,7 @@
+//每次调用$.get()或$.post()或$.ajax()的时候，会先调用ajaxPrefilter
+//这个函数
+
+$.ajaxPrefilter(function (options) {
+    options.url = 'http://api-breakingnews-web.itheima.net' + options.url;
+
+})
---
+++ assets/js/login.js
@@ -0,0 +1,65 @@
+$(function () {
+    //点击“去注册账号”的链接
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide();
+        $('.reg-box').show();
+    })
+
+    //点击“去登陆”的链接
+    $('#link_login').on('click', function () {
+        $('.login-box').show();
+        $('.reg-box').hide();
+    })
+
+    //从 layui 中获取form 对象
+    var form = layui.form;
+    var layer = layui.layer;
+
+    form.verify({
+        pwd: [
+            /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+        ],
+        repwd: function (value) {
+            var pwd = $('.reg-box [name = password]').val();
+            if (pwd !== value) {
+                return '两次密码不一致';
+            }
+        }
+    })
+
+    //监听注册表单的提交事件
+    $('#form_reg').on('submit', function (e) {
+        e.preventDefault();
+        $.post('/api/reguser', {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        }, function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message);
+            }
+            layer.msg(res.message);
+            //模拟人的登录行为
+            $('#link_login').click();
+        });
+    })
+
+    //监听登录表单的提交事件
+    $('#form_login').on('submit', function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败！')
+                }
+                layer.msg('登录成功')
+                //将登录成功得到的token字符串，保存到localStorage中
+                localStorage.setItem('token', res.token)
+                //跳转到后台主页
+                location.href = '/index.html'
+            }
+        })
+    })
+})
